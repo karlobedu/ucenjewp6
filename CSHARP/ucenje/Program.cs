4a6b24ce97ba76f27ea64c60411c58799547e152
Karlo Bilić <karlo.bilic2003@gmail.com>
2024-12-02 17:51:19 +0100

    upoznavanje sa syntaxom

--- CSHARP/ucenje/Program.cs
+++ CSHARP/ucenje/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// komentar jedna linija
+
+/*
+ 
+
+Komentar više linija
+
+
+*/
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozdrav svijetu ščććlčćčččćđšđđđđđđ");
+Console.WriteLine("Hello Osijek");
